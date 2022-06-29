a676f36f225e522d348057bcd350198b0c2dfe48
nolan78 <superpika.nolan@gmail.com>
2022-06-29 16:36:05 +0200

    Modification du visuel connexion

--- src/pages/Connexion.tsx
+++ src/pages/Connexion.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Text, TextInput, TouchableOpacity } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import StorageService from "../services/StorageService";
 
@@ -14,19 +14,26 @@ export default function Connexion({navigation} : {navigation: any}) {
     };
 
     return (
-        <SafeAreaView>
-            <Text>Page Connexion</Text>
-            <TextInput
-                style={{marginTop:'5%', justifyContent: "center", backgroundColor: "#ededed", borderColor: "gray",width: "90%",borderWidth: 1,borderRadius: 10,padding: 10}}
-                onChangeText={setUsername}
-                value={username}
-            />
-            <TouchableOpacity
-                style={{marginTop:'5%', backgroundColor: "#BEAF40", borderRadius: 10, alignItems: 'center', paddingHorizontal: 32, paddingVertical: 12}}
-                onPress={launchTask}
-            >
-                <Text style={{fontSize: 12}}>Se connecter</Text>
-            </TouchableOpacity>
+        <SafeAreaView style={{ flexGrow:1, display: 'flex', alignItems:'center', justifyContent:'center', margin: 20}}>
+            <View style={{display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', width: '100%', backgroundColor: "white", padding:30, margin: 10, borderRadius: 10}}>
+                <View style={{justifyContent: "center", alignItems: "center", backgroundColor: "white", padding: 20, margin: 10}}>
+                <Text style={{fontSize: 17}}> Connexion </Text>
+            </View>
+                <TextInput
+                    style={{marginTop:'5%', justifyContent: "center", borderColor: "gray",width: "90%",borderWidth: 0.5, borderRadius: 10, padding: 10}}
+                    onChangeText={setUsername}
+                    value={username}
+                    placeholder={"Votre login"}
+                />
+                <TouchableOpacity
+                    style={{marginTop:'5%', backgroundColor: "#57A0D2", borderRadius: 10, alignItems: 'center', paddingHorizontal: 32, paddingVertical: 12}}
+                    onPress={launchTask}
+                >
+                    <Text style={{fontSize: 12}}>Se connecter</Text>
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
+    
 }
+
