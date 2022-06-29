d26c7aeb139cade461f7f5900d89a05ae8425170
nolan78 <superpika.nolan@gmail.com>
2022-06-29 09:57:19 +0200

    SafeAreaView ajouter

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Detail({navigation} : {navigation: any}) {
     return (
-        <View style={{paddingTop: "7%"}}>
-            <Text onPress={()=>{navigation.navigate("Home")}}>Page Detail</Text>
-        </View>
+        <SafeAreaView>
+            <View style={{paddingTop: "7%"}}>
+                <Text onPress={()=>{navigation.navigate("Home")}}>Page Detail</Text>
+            </View>
+        </SafeAreaView>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home({navigation} : {navigation: any}) {
     return (
-        <View style={{paddingTop: "7%"}}>
-            <Text onPress={()=>{navigation.navigate("Detail")}}>Page Home</Text>
-        </View>
+        <SafeAreaView>
+            <View style={{paddingTop: "7%"}}>
+                <Text onPress={()=>{navigation.navigate("Detail")}}>Page Home</Text>
+            </View>
+        </SafeAreaView>
     )
 }
