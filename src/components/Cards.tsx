0dade0d81dec836945bee9ce701e0442c3ddaba0
nolan78 <superpika.nolan@gmail.com>
2022-06-29 17:41:12 +0200

    Ne pas supprimer si pas sa note

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,13 +4,21 @@ import Ionicons from "@expo/vector-icons/Ionicons"
 import { useEffect, useState } from "react";
 import { getDateFormated } from "../utils/ConverteDate";
 import NoteService from "../services/NoteService";
+import StorageService from "../services/StorageService";
 
 export default function Cards({note, getList} : {note: INote, getList: Function})
  {
     const [date, setDate] = useState(note.creation_date as any);
+    const [username, setUserName] = useState("");
+
+    const getUser = async () => {
+        const username = await StorageService.getStorage("username");
+        setUserName(username);
+    };
 
     useEffect(()=>{
        const date = getDateFormated(note.creation_date);
+       getUser();
        setDate(date);
     },[])
 
@@ -35,9 +43,11 @@ export default function Cards({note, getList} : {note: INote, getList: Function}
 
     return (
         <View style={{backgroundColor: "white", marginHorizontal: 20, marginVertical: 5, padding:10, borderRadius: 10}}>
-            <View style={{justifyContent: "flex-end", alignItems: "flex-end"}}>
-                <Ionicons onPress={() => createTwoButtonAlert(note._id)} size={15} name="close-outline"></Ionicons>
-            </View>  
+            { note.author === username &&
+                <View style={{justifyContent: "flex-end", alignItems: "flex-end"}}>
+                        <Ionicons onPress={() => createTwoButtonAlert(note._id)} size={15} name="close-outline"></Ionicons>
+                </View>  
+            }
             <Text style={{ fontWeight: "bold", fontSize: 12, fontStyle: "italic", paddingBottom: 5}}>{date}</Text>
             <View style={{flexDirection: 'row'}}>
                 <Image
