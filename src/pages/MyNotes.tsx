9cc6114c6ebedc08b7db4e6f9f66f0649b40c1cc
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-06-29 15:00:41 +0200

    page my notes fini sans les filtres

--- src/pages/MyNotes.tsx
+++ src/pages/MyNotes.tsx
@@ -1,9 +1,34 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, ScrollView } from "react-native";
+import Cards from "../components/Cards";
+import INote from "../interfaces/NoteInterface";
+import NoteService from "../services/NoteService";
+import StorageService from "../services/StorageService";
 
 export default function MyNotes({navigation} : {navigation: any}) {
+    const [noteList, setNoteList] = useState([] as INote[]);
+
+    const getMyNoteList = async () => {
+        const response = await NoteService.getNote();
+        const username = await StorageService.getStorage("username");
+        const listFiltered = response.filter(note => note.author === username);
+        setNoteList(listFiltered);
+    }
+
+    useEffect(()=>{
+        getMyNoteList();
+    }, []);
+
     return (
-        <View>
-            <Text onPress={()=>{navigation.navigate("Connexion")}}>Page MyNotes</Text>
-        </View>
+        <ScrollView>
+            <Text onPress={()=>{navigation.navigate("Connexion")}}>Page Home</Text>
+            {
+                noteList.length ? noteList.map((note:INote, idx:number) =>{
+                    return (
+                        <Cards key={idx} note={note}/>
+                    );
+                }) : null
+            }
+        </ScrollView>
     )
 }
