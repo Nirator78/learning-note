013f79799dcaf9a0108b16517b6e658e2abf298a
nolan78 <superpika.nolan@gmail.com>
2022-06-29 10:22:33 +0200

    Icon

--- App.tsx
+++ App.tsx
@@ -9,28 +9,13 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Detail') {
-            iconName = focused ? 'ios-list-box' : 'ios-list';
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Detail" component={Detail} />
+      <Tab.Navigator>
+        <Tab.Screen options={{tabBarIcon: ({ color }) => (
+          <Ionicons name="home" size={30} color={color}></Ionicons> 
+          ),}} name="Home" component={Home}></Tab.Screen>
+        <Tab.Screen options={{tabBarIcon: ({ color }) => (
+          <Ionicons name="list-outline" size={30} color={color}></Ionicons> 
+          ),}} name="Detail" component={Detail} />
       </Tab.Navigator>
     </NavigationContainer>
   );
