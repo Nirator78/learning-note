860ab56896388d9b53f6c83d64be0d6434fbbebb
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-06-29 14:51:28 +0200

    refacto et completion du service de note

--- src/services/NoteService.ts
+++ src/services/NoteService.ts
@@ -6,8 +6,27 @@ const api = axios.create({baseURL: "https://m66nqp6pe8.eu-west-1.awsapprunner.co
 class NoteService {
     async getNote(): Promise<INote[]> {
         const response = await api.get("/note");
-        const noteList = await response;
-        return noteList.data;
+        return await response.data;
+    }
+
+    async getOneNote(id: string): Promise<INote[]> {
+        const response = await api.get(`/note/${id}`);
+        return await response.data;
+    }
+
+    async createNote(payload: INote): Promise<INote[]> {
+        const response = await api.post("/note", payload);
+        return await response.data;
+    }
+
+    async updateNote(id: string, payload: INote): Promise<INote[]> {
+        const response = await api.put(`/note/${id}`, payload);
+        return await response.data;
+    }
+
+    async deleteNote(id: string) {
+        const response = await api.delete(`/note/${id}`);
+        return await response.data;
     }
 }
 
