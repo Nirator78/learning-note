3c85bae17d71f5c4807ecd4420e8810c071d0c73
nolan78 <superpika.nolan@gmail.com>
2022-06-29 16:59:54 +0200

    Correction filtre

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,6 +19,7 @@ export default function Home() {
     const getNoteList = async () => {
         const response = await NoteService.getNote();
         setNoteList(response);
+        setDisplayNoteList(response);
     }
 
     const filtreNoteListByTag = async (text: string) => {
