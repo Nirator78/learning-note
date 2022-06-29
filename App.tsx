545d7f8235e46decddde1745df85358281ab4780
nolan78 <superpika.nolan@gmail.com>
2022-06-29 09:58:43 +0200

    Import inutile

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaInsetsContext, SafeAreaView } from 'react-native-safe-area-context';
 import Detail from './src/pages/Detail';
 import Home from './src/pages/Home';
 
