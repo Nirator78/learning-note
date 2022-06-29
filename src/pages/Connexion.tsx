634cdd9ef31069a8aac9649e875b7efecd94e3f5
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-06-29 12:42:28 +0200

    page connexion moche mais qui fonctionne

--- src/pages/Connexion.tsx
+++ src/pages/Connexion.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import StorageService from "../services/StorageService";
 
 export default function Connexion({navigation} : {navigation: any}) {
+    const [username, setUsername] = useState("");
+
+    const launchTask = async () => {
+        if(username){
+            StorageService.setStorage("username", username);
+            navigation.navigate("BottomTabNavigation")
+        }
+    };
+
     return (
         <SafeAreaView>
-            <Text onPress={()=>{navigation.navigate("BottomTabNavigation")}}>Page Connexion</Text>
+            <Text>Page Connexion</Text>
+            <TextInput
+                style={{marginTop:'5%', justifyContent: "center", backgroundColor: "#ededed", borderColor: "gray",width: "90%",borderWidth: 1,borderRadius: 10,padding: 10}}
+                onChangeText={setUsername}
+                value={username}
+            />
+            <TouchableOpacity
+                style={{marginTop:'5%', backgroundColor: "#BEAF40", borderRadius: 10, alignItems: 'center', paddingHorizontal: 32, paddingVertical: 12}}
+                onPress={launchTask}
+            >
+                <Text style={{fontSize: 12}}>Se connecter</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
