f66a92efdf7a97a8e44039af685fcb968d4a3e61
nolan78 <superpika.nolan@gmail.com>
2022-06-30 12:59:17 +0200

    Edit

--- src/components/CardsDetail.tsx
+++ src/components/CardsDetail.tsx
@@ -46,12 +46,16 @@ export default function CardsDetails({note} : {note: INote}) {
         navigation.navigate("Home");
     };
 
+    const goToModification = () => {
+        navigation.navigate("Formulaire", {note: note});
+    };
+
     return (
         <View style={{ backgroundColor: "white", marginHorizontal: 20, marginVertical: 5, padding:10, borderRadius: 10}}>           
             {
                 username === note.author ? (
                 <View style={{justifyContent: "flex-end", alignItems: "flex-end",}}>
-                    <BasicButton onPress={() => createTwoButtonAlert(note._id)}>
+                    <BasicButton onPress={() => goToModification()}>
                         <Ionicons style={{ marginEnd:-20 }} size={20} name="create-outline"></Ionicons>
                     </BasicButton>
                 </View> 
--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -7,7 +7,6 @@ export default function Detail({navigation, route} : {navigation: any, route: an
     const [note, setNote] = useState({} as INote)
 
     useEffect(() => {
-        console.log(route.params.note)
         setNote(route.params.note);
     }, []);
 
